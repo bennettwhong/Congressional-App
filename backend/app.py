6ee72fea5756fea32556821b4ac3ff3791ea2989
bennettwhong <bennettwhong@gmail.com>
2025-10-25 13:33:14 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -54,5 +54,5 @@ def upload_audio():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
 
